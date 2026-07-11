cd421b19f108eef6c23d0fa36deb3f5962ecf8a2
Regine A. Mendoza <mendozaregine500@gmail.com>
2026-07-11 04:32:51 +0000

    Fix: Add missing properties to Candidate model (Qualifications, LeadershipExperience, VotesAllowed)

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VoteHub.Models
@@ -35,14 +35,24 @@ namespace VoteHub.Models
         public required string YearLevel { get; set; }
 
         [StringLength(500)]
-        public required string Biography { get; set; }
+        public string Biography { get; set; } = string.Empty;
 
         [StringLength(1000)]
-        public required string Platform { get; set; }
+        public string Platform { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string Qualifications { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string LeadershipExperience { get; set; } = string.Empty;
 
         [Required]
         [StringLength(255)]
         public required string Photo { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int VotesAllowed { get; set; } = 1;
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
